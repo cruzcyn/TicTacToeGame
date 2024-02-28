a6f0998dbd56b1c0ae19e125b20b5a03cd2098c1
cynthiacruz <ccruzgz@gmail.com>
2024-02-28 16:06:53 -0600

    Remove import statements and extra files to simplify the game.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import random
-import tkinter
-from gameboard import GameBoard
-from gamelogic import GameLogic
+board = [
+    "-", "-", "-",
+    "-", "-", "-",
+    "-", "-", "-",
+]
 
-board = GameBoard()
-game = GameLogic()
+winner = None
+playing = True
+
+# TODO: print gameboard
+# TODO: take player input
+# TODO: check if space in board is empty
+# TODO: check for win or tie
+# TODO: switch player
+# TODO: loop until someone wins or there's a tie
