9eaebd1588b787e62bcfbcb5086c398dc1ae12fa
cynthiacruz <ccruzgz@gmail.com>
2024-02-28 17:00:57 -0600

    Got rid of the switch user function and edited the rest of the code accordingly.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ board = [
     [" ", " ", " "],
 ]
 
+current_player = 1
 winner = None
 playing = True
 
@@ -12,7 +13,7 @@ playing = True
 # If done inside of a function and split into different lines of print statements, we can print something to the console that looks like a board
 # while preventing writing so many repetitive lines of code throughout the game
 
-def printBoard(game_board):
+def print_board(game_board):
     """ Prints the list that represents the gameboard, separating each item so that what is printed looks like the actual gameboard."""
     print(game_board[0][0] + " | " + game_board[0][1] + " | " + game_board[0][2])
     print("---------")
@@ -20,34 +21,48 @@ def printBoard(game_board):
     print("---------")
     print(game_board[2][0] + " | " + game_board[2][0] + " | " + game_board[2][0])
 
-printBoard(board)
 
-# TODO: take player input
-
+def switch_player(current_player):
+    """Checks to see whose turn is it currently and re-sets the variable to represent the other player."""
+    if current_player == 1:
+        current_player = 2
+    else:
+        current_player = 1
 
 # GAME LOGIC IMPLEMENTATION
 
 # Greet user
 print("Welcome to TicTacToe!")
-
 # Game loop
 while playing:
-    printBoard(board)
+    print_board(board)
     
     # Ask for player input
     current_player_input = input("In which row and column, respectively, would you like to make your move? ")
-    row = int(current_player_input.split(",")[0])
-    column = int(current_player_input.split(",")[1])
 
-    # TODO: check if space in board is empty
+    try:
+        row = int(current_player_input.split(",")[0])
+        column = int(current_player_input.split(",")[1])
+    except ValueError:
+        print("Invalid input. Your answer must be given in numbers formatted as such: 'row, column'")
+    
     if board[row][column] == " ":
-        
+        if current_player == 1:
+            board[row][column] = "X"
+            current_player = 2
+        else:
+            board[row][column] = "O"
+            current_player = 1
+    else:
+        print("Sorry, that spot's already taken! Choose another.")
 
-    # Safe exit from while loop for testing purposes:
-    playing = False
-    print("Bye!")
+    # "Secret password" to exit while loop for testing purposes:
+    if current_player_input == "exit":
+        playing = False
+        print("Bye!")
 
-# TODO: check if space in board is empty
+
+
 # TODO: check for win or tie
 # TODO: switch player
 # TODO: loop until someone wins or there's a tie
